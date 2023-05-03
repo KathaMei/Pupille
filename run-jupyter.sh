2c9a64d5af179962b11b1801b4d0a8a9da043f6a
phoyd <github@phoyd.net>
2023-05-03 11:24:47 +0200

    Provide virtual environment for the code

---
+++ run-jupyter.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+./run.sh jupyter-lab $@
+
+
+
+
---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+if [ ! -d venv ]
+then 
+ python3 -m venv venv
+ . venv/bin/activate
+ pip install -r requirements.txt
+else
+ . venv/bin/activate
+fi
+if [ $# -eq 0 ] 
+then 
+ echo "Starte neue Shell ($SHELL)"
+ $SHELL
+else 
+ echo "Starte Kommando $@"
+ $@
+fi
+
+
+
+
