52619103c5cd8edaa13f988c03adc6943ff46e8e
Marco Manfredini <marco@phoyd.net>
2023-07-17 22:23:40 +0200

    Handle A and B (Baseline) in get_condition

--- Pupillengröße/Skripte/pup_util.py
+++ Pupillengröße/Skripte/pup_util.py
@@ -16,7 +16,11 @@ def get_condition(subject_id):
     zuordnungen=f'{os.path.dirname(__file__)}/zuordnungen.csv'
     f=pd.read_csv(zuordnungen,index_col='proband')
     prob=subject_id[:4]
-    msr=subject_id[5:6]    
+    msr=subject_id[5:6]
+    if msr=='A': 
+        return (0,"BaselineA")
+    if msr=='B': 
+        return (0,"BaselineB")
     q=f.loc[prob][int(msr)-1]
     names=[("3.4","3.4Stim"),("3.4","3.4Placebo"),("30","30Stim"),("30","30Placebo")]
     return names[int(q)-1]
